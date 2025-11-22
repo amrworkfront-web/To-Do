e8af3cfdf1d888e8b4065ff2bcc645aeade884bf
Amr <amrworkfront@gmail.com>
2025-11-22 20:37:35 +0200

    refactor: streamline axios client configuration and improve authorization handling

--- app/_utils/axiosClient.ts
+++ app/_utils/axiosClient.ts
@@ -1,13 +1,13 @@
-import axios from 'axios';
 
-const apiUrl = "http://localhost:1337/api";
 
-const axiosClient = axios.create({
-  baseURL: apiUrl,
-});
+import axios from 'axios'
+const apiKey=process.env.NEXT_PUBLIC_REST_API_KEY
+const apiUrl="http://localhost:1337/api"
 
-export const setAuthToken = (token: string) => {
-  axiosClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-};
-
-export default axiosClient;
+const axiosClient= axios.create({
+    baseURL:apiUrl,
+    headers:{
+        Authorization:`Bearer ${apiKey}`
+    }
+})
+export default axiosClient
