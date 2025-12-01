2fc65d1f661ba3a768c76688832b71a4a5cf79a2
Amr <amrworkfront@gmail.com>
2025-12-01 22:50:36 +0200

    moved navbar and header from page.tsx to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,26 @@
-import Navbar from "./components/navbar";
 import StatsCard from "./components/stats-card";
 import AddTask from "./components/add-tsk";
 import RightPanel from "./components/right-panel";
 import TasksList from "./components/tasks-list";
-import Sidebar from "./components/sidebar";
 
-
-export default   function Home() {
-
-
-
+export default function Home() {
   return (
-    
-<div className="flex">
-  <div>
-            <Sidebar />
+    <div className="flex">
 
-  </div>
       <div className="w-full  ">
-      <Navbar></Navbar>
-      <div className="p-6">
-        <div className="grid grid-cols-8 gap-6">
-          <div className="col-span-6 space-y-4">
-            <StatsCard></StatsCard>
-            <AddTask></AddTask>
-            <TasksList></TasksList>
-          </div>
-          <div className="hidden md:block md:col-span-2">
-            <RightPanel></RightPanel>
+        <div className="p-6">
+          <div className="grid grid-cols-8 gap-6">
+            <div className="col-span-6 space-y-4">
+              <StatsCard></StatsCard>
+              <AddTask></AddTask>
+              <TasksList></TasksList>
+            </div>
+            <div className="hidden md:block md:col-span-2">
+              <RightPanel></RightPanel>
+            </div>
           </div>
         </div>
       </div>
     </div>
-</div>
   );
 }
- 
